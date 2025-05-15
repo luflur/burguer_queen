3fad8045377039321f551e2e213102573f834269
luflur <lpt2001-2001@hotmail.com>
2025-05-15 11:14:50 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Burguer Queen',
+  webDir: 'www'
+};
+
+export default config;
